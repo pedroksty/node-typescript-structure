e2180191a846fdb5d88db55e6bcfb9923dd2d309
pedroksty <pedrotrab2132@gmail.com>
2020-06-26 20:46:06 -0300

    Add UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+
+class UserController {
+  async store(request: Request, response: Response) {
+
+  }
+
+  async index(request: Request, response: Response) {
+
+  }
+
+  async show(request: Request, response: Response) {
+
+  }
+
+  async update(request: Request, response: Response) {
+
+  }
+  async delete(request: Request, response: Response) {
+
+  }
+
+}
+
+export default new UserController()
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import UserController from '@controllers/UserController'
+
+
 
 const router = Router()
 
 router.get('/', (request, response) => {
   response.json({ SERVER: 'ON' })
 })
+
+router.post('/users', UserController.store)
+router.get('/users', UserController.index)
+router.get('/users/:id', UserController.show)
+router.delete('/users', UserController.delete)
+router.put('/users', UserController.update)
+
 
 export default router
