ebd62754026322e32fd4c2916b2c9aba7bb3371e
pedroksty <pedrotrab2132@gmail.com>
2021-02-25 10:18:19 -0300

    Add Babel config

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@config': './src/config',
+        '@models': './src/models',
+        '@controllers': './src/controllers',
+        '@views': './src/views',
+        '@modules': './src/modules',
+        '@shared': './src/shared'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
